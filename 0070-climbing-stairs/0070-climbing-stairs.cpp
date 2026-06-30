a35ed896408668f4dd1c6f7dd21eaf64c6e6d242
kriti0713 <162013022+kriti0713@users.noreply.github.com>
2026-06-30 13:29:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.22%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+
+        int step1 = 1;
+        int step2 = 2;
+
+        for(int i = 3; i<=n; i++){
+            int current = step1 + step2;
+            step1 = step2;
+            step2 = current;
+        }
+        return step2;
+    }
+};
