0818152ba1344b8a5907f894c78bbe3761da20d9
kriti0713 <162013022+kriti0713@users.noreply.github.com>
2026-07-01 23:00:30 +0530

    Time: 438 ms (45.19%), Space: 142.4 MB (48.62%) - LeetHub

---
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    long long helper(vector<int>& prices, int i, int state, int k,
+               vector<vector<vector<long long>>>& memo) {
+        if(k==0) return 0;      
+        if (i == prices.size()){
+            if(state == 0){
+                return 0;
+            }
+            else{
+                return -1e12;
+            }
+        }
+            
+        if (memo[i][state][k] != -1){
+            return memo[i][state][k];
+        }
+        long long profit = 0;
+        if (state == 0) {
+            long long buy = -prices[i] + helper(prices, i + 1, 1, k,memo);
+            long long sellshort = prices[i] + helper(prices, i+1, 2, k, memo);
+            long long skip = helper(prices, i + 1, 0, k, memo);
+            profit = max({buy, sellshort, skip});
+        } else if(state == 1) {
+            long long sell = prices[i] + helper(prices, i + 1, 0, k -1, memo);
+            long long skip = helper(prices, i + 1, 1, k, memo);
+            profit = max(sell, skip);
+        }
+        else{
+            long long buyback = -prices[i] + helper(prices, i+1, 0, k-1, memo);
+            long long skip = helper(prices, i+1, 2, k, memo);
+            profit = max(buyback, skip);
+        }
+        return memo[i][state][k] = profit;
+    }
+    long long maximumProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        if(n == 0 || k == 0) return 0;
+        k = min(k,n/2);
+        
+        vector<vector<vector<long long>>> memo(n , vector<vector<long long>>(3, vector<long long>(k+1,-1)));
+        return helper(prices, 0,0,k,memo);
+    }
+};
