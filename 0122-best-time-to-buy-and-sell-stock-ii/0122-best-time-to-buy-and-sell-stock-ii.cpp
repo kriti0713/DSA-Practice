d929a649a418f67594425745caed1804a2452667
kriti0713 <162013022+kriti0713@users.noreply.github.com>
2026-07-01 19:39:32 +0530

    Time: 12 ms (22.78%), Space: 27.6 MB (9.16%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,38 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int total = 0;
+    int helper(vector<int>& prices,int i, int state, vector<vector<int>>& memo){
+        if(i == prices.size()) return 0;
+        if(memo[i][state] != -1) return memo[i][state];
 
-        for(int i =1; i<prices.size(); i++){
-            total += max(0, prices[i] - prices[i-1]);
+        int profit = 0;
+        if(state == 0 ){
+            int buy = -prices[i] + helper(prices, i + 1, 1, memo);
+            int skip = helper(prices, i + 1, 0, memo);
+            profit = max(buy, skip);
         }
-        return total;
+        else{
+            int sell = prices[i] + helper(prices, i + 1, 0, memo);
+            int skip = helper(prices, i + 1, 1, memo);
+            profit = max(sell,skip);
+        }
+        return memo[i][state] = profit;
+        
+    }
+    
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> memo(n , vector<int>(2,-1));
+        return helper(prices, 0,0,memo);
+
+
+
+        // int total = 0;
+
+        // for(int i =1; i<prices.size(); i++){
+        //     total += max(0, prices[i] - prices[i-1]);
+        // }
+        // return total;
         
     }
 };
