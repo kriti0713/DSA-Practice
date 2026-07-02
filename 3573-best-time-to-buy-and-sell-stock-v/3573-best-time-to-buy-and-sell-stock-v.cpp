21c2d991def7d734e43aaaab771855a3f8f50b32
kriti0713 <162013022+kriti0713@users.noreply.github.com>
2026-07-02 14:23:16 +0530

    Time: 436 ms (44.58%), Space: 142.4 MB (48.34%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    long long helper(vector<int>& prices, int i, int state, int k,
-               vector<vector<vector<long long>>>& memo) {
+    long long helper(vector<int>& prices, int i, int state, int k,vector<vector<vector<long long>>>& memo) {
         if(k==0) return 0;      
         if (i == prices.size()){
-            if(state == 0){
-                return 0;
-            }
-            else{
-                return -1e12;
-            }
+           if(state == 0){
+            return 0;
+           }
+           else{
+            return -1e15;
+           }
         }
             
         if (memo[i][state][k] != -1){
@@ -18,11 +17,11 @@ public:
         long long profit = 0;
         if (state == 0) {
             long long buy = -prices[i] + helper(prices, i + 1, 1, k,memo);
-            long long sellshort = prices[i] + helper(prices, i+1, 2, k, memo);
+            long long shortsell = prices[i] + helper(prices, i+1, 2, k, memo);
             long long skip = helper(prices, i + 1, 0, k, memo);
-            profit = max({buy, sellshort, skip});
+            profit = max({buy, shortsell, skip});
         } else if(state == 1) {
-            long long sell = prices[i] + helper(prices, i + 1, 0, k -1, memo);
+            long long sell = prices[i] + helper(prices, i + 1, 0, k-1 , memo);
             long long skip = helper(prices, i + 1, 1, k, memo);
             profit = max(sell, skip);
         }
@@ -35,8 +34,9 @@ public:
     }
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
-        if(n == 0 || k == 0) return 0;
-        k = min(k,n/2);
+        
+        
+       
         
         vector<vector<vector<long long>>> memo(n , vector<vector<long long>>(3, vector<long long>(k+1,-1)));
         return helper(prices, 0,0,k,memo);
