5161cba2ee698216e1b29cfcad1b64fa13b8bf48
kriti0713 <162013022+kriti0713@users.noreply.github.com>
2026-07-02 15:42:30 +0530

    Time: 163 ms (42.97%), Space: 106.7 MB (19.52%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,32 +1,31 @@
 class Solution {
+ int solve(int k, int m, int n, vector<string>& strs, vector<vector<vector<int>>>& memo) {
+    if(k == strs.size()) return 0;
+
+    if (memo[k][m][n] != -1) return memo[k][m][n];
+
+       int dontpick = solve(k + 1, m, n, strs, memo);
+       int pick = 0;
+
+       int zeros = 0;
+       int ones = 0;
+       for (char c : strs[k]) {
+          if (c == '0') zeros++;
+            else ones++;
+        }
+
+        if (m >= zeros && n >= ones) {
+            pick = 1 + solve(k + 1, m - zeros, n - ones, strs, memo); 
+        }
+
+        return memo[k][m][n] = max(dontpick, pick);
+ }      
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
 
-        vector<vector<vector<int>>> dp(
-            len + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, 0)));
-
-        for (int k = 1; k <= len; k++) {
-            int zeros = 0;
-            int ones = 0;
-
-            for (char c : strs[k - 1]) {
-                if (c == '0')
-                    zeros++;
-                else
-                    ones++;
-            }
-
-            for (int i = 0; i <= m; i++) {
-                for (int j = 0; j <= n; j++) {
-                    dp[k][i][j] = dp[k - 1][i][j];
-
-                 if (i >= zeros && j >= ones) {
-                    dp[k][i][j] = max(dp[k][i][j], dp[k - 1][i - zeros][j - ones] + 1);
-                 }     
-            }
-        }
-    }    
-         return dp[len][m][n];   
+        vector<vector<vector<int>>> memo(len+1 , vector<vector<int>>(m+1, vector<int>(n+1,-1)));
+       
+         return solve(0, m, n, strs,memo);
         }
     };
