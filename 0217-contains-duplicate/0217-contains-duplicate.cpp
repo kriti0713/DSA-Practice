118ceb7042f01a0bb2d3f74477946c312e693f63
kriti0713 <162013022+kriti0713@users.noreply.github.com>
2026-07-03 23:25:29 +0530

    Time: 31 ms (80.46%), Space: 82.7 MB (78.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] == nums[i - 1]) {
